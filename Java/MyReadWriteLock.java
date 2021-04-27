f854f9346c964382683fc6f50112ca63bfd54770
yaquan.zhou <yaquan.zhou@yitu-inc.com>
2021-04-27 12:07:33 +0800

    add my 读写锁

---
+++ Java/MyReadWriteLock.java
@@ -0,0 +1,44 @@
+package concurrency.lock;
+
+public class MyReadWriteLock {
+    //读写锁的共享变量
+    private int state = 0;
+
+    // 高16位为读锁
+    private int getReadCount() {
+        return state >>> 16;  //无符号右移
+    }
+
+    // 低16位为写锁
+    private int getWriteCount() {
+        return state & (1 << 16 - 1);
+    }
+
+    public synchronized void lockRead() throws InterruptedException {
+        while (getWriteCount() > 0) {
+            wait();
+        }
+        System.out.println("lockRead ---" + Thread.currentThread().getName());
+        state += 1 << 16;
+    }
+
+    public synchronized void unLockRead() throws InterruptedException {
+        state -= 1 << 16;
+        System.out.println("unLock Read ---" + Thread.currentThread().getName());
+        notifyAll();
+    }
+
+    public synchronized void lockWrite() throws InterruptedException {
+        while (getWriteCount() > 0 || getReadCount() > 0) {
+            wait();
+        }
+        System.out.println("lockWrite ---" + Thread.currentThread().getName());
+        state += 1;
+    }
+
+    public synchronized void unLockWrite()  {
+        state -= 1;
+        System.out.println("unLock write ---" + Thread.currentThread().getName());
+        notifyAll();
+    }
+}
